4419b82f2f7b741e11212273db11758132be48dd
Andras Pal <andr4s.pal@gmail.com>
2019-08-22 19:09:26 +0100

    AuthService / registerUser added

---
+++ Slacky/Services/AuthService.swift
@@ -0,0 +1,70 @@
+//
+//  AuthService.swift
+//  Slacky
+//
+//  Created by Andras Pal on 22/08/2019.
+//  Copyright © 2019 Andras Pal. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn : Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            if response.result == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+}
--- Slacky/Utilities/Constants.swift
+++ Slacky/Utilities/Constants.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+
+let BASE_URL = "https://slackyslackyslackslack.herokuapp.com/"
+
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 
 let TO_LOGIN = "toLogin"
@@ -15,3 +23,13 @@ let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 
 let UNWIND = "unwindToChannel"
+
+
+
+// User Defaults
+
+let TOKEN_KEY = "token"
+
+let LOGGED_IN_KEY = "loggedIn"
+
+let USER_EMAIL = "userEmail"
