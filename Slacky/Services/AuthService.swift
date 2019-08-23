f5fde5dd2c2695d77081a0f4d477809877ccbce0
Andras Pal <andr4s.pal@gmail.com>
2019-08-23 16:58:53 +0100

    rounded button class + registering a user

--- Slacky/Services/AuthService.swift
+++ Slacky/Services/AuthService.swift
@@ -54,7 +54,7 @@ class AuthService {
         ]
         
         Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
-            if response.result == nil {
+            if response.result.error == nil {
                 completion(true)
             } else {
                 completion(false)
---
+++ Slacky/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  Slacky
+//
+//  Created by Andras Pal on 23/08/2019.
+//  Copyright © 2019 Andras Pal. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
